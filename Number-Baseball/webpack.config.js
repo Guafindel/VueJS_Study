a1fefed623f97dada18373cde7b4d865a370645b
Guafindel <50958613+Guafindel@users.noreply.github.com>
2019-12-29 18:18:01 +0900

    12-28 / 18:17
    웹팩의 기본 구조 webpack.config.js 작성

---
+++ Number-Baseball/webpack.config.js
@@ -0,0 +1,15 @@
+module.export = {
+    entry: {
+        app: './main.js',
+    },
+    module: {
+        rules: [{
+
+        }],
+    },
+    plugins: [],
+    output: {
+        filename: '[name].js',
+        path: './dist',
+    },
+};
